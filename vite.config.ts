f89cdea3e040423e3fb7478a4b5e5f7f5a3d3bea
mahfudhpakku <mahfudh552@gmail.com>
2026-03-06 11:04:33 +0700

    Update Build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // Tambahkan baris ini! Penting agar link CSS/JS tidak pecah (404)
   base: '/Arus-Kas-App/', 
+  build: {
+    outDir: 'dist',
+  },
   
   plugins: [
     react(),
